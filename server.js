b4982e046810cd1f0b47e37ac0142c379417c2f6
Kmart002 <128054511+Kmart002@users.noreply.github.com>
2023-09-20 09:51:46 +0100

    jamor

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(express.json())
 const usersRouter = require('./routes/route')
  app.use('/users', usersRouter)
  
- app.get('/api', (req,res)=>{
-res.json('success')
- })
+//  app.get('/api', (req,res)=>{
+// res.json('success')
+//  })
 
 
 app.listen(port,() =>console.log(`server is running on port ${port}`) )
